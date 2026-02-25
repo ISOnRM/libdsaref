c840eff918cdeba9f2197faab63e0dc1f3971fe4
Mark Ison <markison7002@gmail.com>
2026-02-25 21:52:16 +0200

    Add errno to parse_any macro

--- include/dsaref/parse_any.h
+++ include/dsaref/parse_any.h
@@ -2,12 +2,13 @@
 #define PARSE_ANY_H
 
 #include <stdio.h>
+#include <errno.h>
 
 #define DEF_PARSE_ANY(type, fmt)                            \
 static inline int parse_##type(const char *s, type *out) {  \
     if (!s || !out) return -1;                              \
     type t;                                                 \
-    if (sscanf(s, fmt, &t) != 1) return -1;                 \
+    if (sscanf(s, fmt, &t) != 1) {errno=EINVAL; return -1;};\
     *out = t;                                               \
     return 0;                                               \
 }                                                            
