36119010cd6f47a0c219cc296d621c654ff7950b
Mark Ison <markison7002@gmail.com>
2026-02-27 04:33:18 +0200

    Add queue

---
+++ include/dsaref/queue.h
@@ -0,0 +1,35 @@
+// queue header
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include <stddef.h>
+#include "dsaref/vec.h"
+
+struct queue {
+    size_t head, tail, count, cap;
+    vec v;
+};
+
+typedef struct queue queue;
+
+/* lifecycle */
+int queue_init(queue *q, size_t elem_size, size_t cap); // 0 or -1
+void queue_destroy(queue *q);
+
+/* enqueue and dequeue and clear */
+int queue_enq(queue *q, const void *elem); // 0 or -1
+int queue_deq(queue *q, void *out); // 0 or -1
+int queue_clear(queue *q); // 0 or -1
+
+/* fields */
+#ifdef QUEUE_FIELDS_W_MACROS
+#define queue_empty(q) (((q)-> count == 0) ? 1 : 0)
+#else
+int queue_empty(const queue *q); // 1/0 or -1
+#endif
+
+
+
+
+
+#endif // QUEUE_H
---
+++ src/queue.c
@@ -0,0 +1,58 @@
+// queue impl
+#include <stddef.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include "dsaref/queue.h"
+
+int queue_init(queue *q, size_t elem_size, size_t cap) {
+    if (!q || elem_size == 0 || cap == 0) { errno = EINVAL; return -1; }
+    if (vec_init(&q->v, elem_size) != 0) return -1;
+    q->count = q->head = q->tail = 0;
+    q->cap = cap;
+    return 0; 
+}
+
+
+void queue_destroy(queue *q) {
+    if (!q) return;
+    vec_destroy(&q->v);
+    q->cap = q->count = q->head = q->tail = 0;
+}
+
+int queue_empty(const queue *q) {
+    if (!q) { errno = EINVAL; return -1; }
+    return (q->count == 0) ? 1 : 0;
+}
+
+
+int queue_enq(queue *q, const void *elem) {
+    if (!q || !elem) { errno = EINVAL; return -1; }
+    if (q->cap == q->count)          { return -1; }
+    void *dst = (char*)q->v.data + q->tail * q->v.elem_size;
+    memcpy(dst, elem, q->v.elem_size);
+    
+    q->tail = (q->tail + 1) % q->cap;
+    q->count++;
+
+    return 0;
+}
+int queue_deq(queue *q, void *out) {
+    if (!q) { errno = EINVAL; return -1; }
+    if (q->count == 0)      { return -1; }
+    if (out) {
+        void *src = (char *)q->v.data + q->head * q->v.elem_size;
+        memcpy(out, src, q->v.elem_size);
+    }
+
+    q->head = (q->head + 1) % q->cap;
+    q->count--;
+
+    return 0;
+}
+int queue_clear(queue *q) {
+    if (!q) { errno = EINVAL; return -1; }
+    q->cap = q->count = q->head = q->tail = 0;
+    q->v.len = 0;
+    return 0;
+}
